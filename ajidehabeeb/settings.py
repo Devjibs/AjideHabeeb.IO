c2a7bce9f0188e1d8bb59f02383e22546d19f0bf
JiboGithub <ajidejibola@yahoo.com>
2021-08-22 16:20:36 +0100

    update

--- ajidehabeeb/settings.py
+++ ajidehabeeb/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path, os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,11 +118,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR /'static'
+# STATICFILES_DIRS = [
+#     'ajidehabeeb/static',
+# ]
+
+
+
+
+
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+# STATICFILES_DIRS = [
+#     'ajidehabeeb/static',
+
+    
 STATICFILES_DIRS = [
     'ajidehabeeb/static',
 ]
+
 
 
 #Configuration for media files
